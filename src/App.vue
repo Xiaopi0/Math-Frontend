dce2cfb9f18a964d3ab91387f184a29efe512628
Xiaopi0 <erikkirkegaardstage@gmail.com>
2021-05-12 11:27:39 +0200

    1 file modified, 5 deleted

--- src/App.vue
+++ src/App.vue
@@ -1,14 +1,6 @@
 <template>
   <div class="container" id="app">
     <Section Title="Math Frontend" Text="A Webpage for my solutions to Math Tasks" />
-    <form id="theform" v-on:submit="onSubmit(message)">
-      <p>Input opgname. format: {type fx. FP9}_{dd}_{mm}_{yyyy}.json</p>
-      <input class="theonlyfuckinginputonthepage" v-model="message" /><br />
-      <input type="submit" value="submit" />
-    </form>
-    <div class="opgave" id="opgave">
-      {{opgaveTekst}}
-    </div>
   </div>
 </template>
 
